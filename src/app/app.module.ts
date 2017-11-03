c4b675a472ce29de8dfc82575e5b3ac6d90e547f
Mikhail Budarin <miffael.jo@gmail.com>
2017-11-03 10:16:06 +0200

    add 2 tab - hero list and hero detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,  } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { MatListModule, MatGridListModule, MatCardModule, MatToolbarModule, MatButtonModule, MatCheckboxModule, MatInputModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+import { CardHeroComponent } from './components/card-hero/card-hero.component';
+import { ListHeroComponent } from './components/list-hero/list-hero.component';
+
+const MatModules = [
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatInputModule
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardHeroComponent,
+    ListHeroComponent,
   ],
   imports: [
-    BrowserModule
+    ...MatModules,
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
